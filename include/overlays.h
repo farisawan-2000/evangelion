4688507d9266177847b597500d1201b2fac6fd43
someone2639 <someone2639@gmail.com>
2023-11-25 22:48:16 -0500

    match an overlay loading function

--- include/overlays.h
+++ include/overlays.h
@@ -7,16 +7,16 @@
         extern u8 *nm ## _BSS_END;
 
 typedef struct _Overlay {
-    u8 *romStart;
-    u8 *romEnd;
-    u8 *dramStart;
-    u8 *dramEnd;
-    u8 *textStart;
-    u8 *textEnd;
-    u8 *dataStart;
-    u8 *dataEnd;
-    u8 *bssStart;
-    u8 *bssEnd;
+    /* 0x000 */ u8 *romStart;
+    /* 0x004 */ u8 *romEnd;
+    /* 0x008 */ u8 *dramStart;
+    /* 0x00C */ u8 *dramEnd;
+    /* 0x010 */ u8 *textStart;
+    /* 0x014 */ u8 *textEnd;
+    /* 0x018 */ u8 *dataStart;
+    /* 0x01C */ u8 *dataEnd;
+    /* 0x020 */ u8 *bssStart;
+    /* 0x024 */ u8 *bssEnd;
 } Overlay; // size: 0x28
 extern Overlay gOverlayTable[]; // 800D4F50
 
--- src/code_162C0.c
+++ src/code_162C0.c
@@ -1,29 +1,30 @@
+// c_flag_g2
 #include "common.h"
+#include "overlays.h"
 
-INCLUDE_ASM("asm/nonmatchings/code_162C0", func_800AB6C0);
-// void func_800AB6C0(s16 arg0) {
-//     u32 textStart;
-//     u32 dataStart;
-//     u32 dataSize;
-//     u32 dramStart;
-//     u32 bssStart;
-//     u32 bssSize;
-//     u32 romStart;
-//     u32 romSize;
+void func_800AB6C0(s16 arg0) {
+    u32 textStart;
+    u32 dataStart;
+    u32 dataSize;
+    u32 dramStart;
+    u32 bssStart;
+    u32 bssSize;
+    u32 romStart;
+    u32 romSize;
 
-//     romStart = gOverlayTable[arg0].romStart;
-//     textStart = gOverlayTable[arg0].textStart;
-//     bssStart = gOverlayTable[arg0].bssStart;
-//     dramStart = gOverlayTable[arg0].dramStart;
-//     dataStart = gOverlayTable[arg0].dataStart;
-//     romSize = gOverlayTable[arg0].romEnd - romStart;
-//     dataSize = gOverlayTable[arg0].dataEnd - dataStart;
-//     bssSize = gOverlayTable[arg0].bssEnd - bssStart;
-//     func_800BD3C0(textStart, gOverlayTable[arg0].textEnd - textStart);
-//     osInvalDCache(dataStart, dataSize);
-//     func_80099174(romStart, dramStart, romSize);
-//     bzero(bssStart, bssSize);
-// }
+    romStart = gOverlayTable[arg0].romStart;
+    romSize = gOverlayTable[arg0].romEnd - romStart;
+    dramStart = gOverlayTable[arg0].dramStart;
+    textStart = gOverlayTable[arg0].textStart;
+    dataStart = gOverlayTable[arg0].dataStart;
+    dataSize = gOverlayTable[arg0].dataEnd - dataStart;
+    bssStart = gOverlayTable[arg0].bssStart;
+    bssSize = gOverlayTable[arg0].bssEnd - bssStart;
+    func_800BD3C0(textStart, gOverlayTable[arg0].textEnd - textStart);
+    osInvalDCache(dataStart, dataSize);
+    func_80099174(romStart, dramStart, romSize);
+    bzero(bssStart, bssSize);
+}
 
 INCLUDE_ASM("asm/nonmatchings/code_162C0", func_800AB7CC);
 
