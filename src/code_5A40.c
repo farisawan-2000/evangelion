e75126de14e0fa8ce25e319cf1d1cf832d5b51c2
someone2639 <someone2639@gmail.com>
2023-11-25 23:23:35 -0500

    migrate sound data to C

--- src/code_5A40.c
+++ src/code_5A40.c
@@ -1,6 +1,108 @@
 // c_flag_g2
 #include "common.h"
 
+typedef struct {
+    u8 *romStart;
+    u8 *romEnd;
+} BankTable;
+
+#define EXTERN_SOUND(nm) \
+        extern u8 *Sound_ ## nm ## _wbk_ROM_START; \
+        extern u8 *Sound_ ## nm ## _wbk_ROM_END; \
+        extern u8 *Sound_ ## nm ## _ptr_ROM_START; \
+        extern u8 *Sound_ ## nm ## _ptr_ROM_END; \
+        extern u8 *Sound_ ## nm ## _tbl_ROM_START; \
+        extern u8 *Sound_ ## nm ## _tbl_ROM_END; \
+        extern u8 *Sound_ ## nm ## _bfx_ROM_START; \
+        extern u8 *Sound_ ## nm ## _bfx_ROM_END;
+
+EXTERN_SOUND(effects);
+EXTERN_SOUND(voice);
+EXTERN_SOUND(sndfnt1);
+EXTERN_SOUND(sndfnt2);
+
+EXTERN_SOUND(soundbank_01);
+EXTERN_SOUND(soundbank_02);
+EXTERN_SOUND(soundbank_03);
+EXTERN_SOUND(soundbank_04);
+EXTERN_SOUND(soundbank_05);
+EXTERN_SOUND(soundbank_06);
+EXTERN_SOUND(soundbank_07);
+EXTERN_SOUND(soundbank_08);
+EXTERN_SOUND(soundbank_09);
+EXTERN_SOUND(soundbank_10);
+EXTERN_SOUND(soundbank_11);
+EXTERN_SOUND(soundbank_12);
+EXTERN_SOUND(soundbank_13);
+EXTERN_SOUND(soundbank_14);
+EXTERN_SOUND(soundbank_15);
+EXTERN_SOUND(soundbank_16);
+EXTERN_SOUND(soundbank_17);
+EXTERN_SOUND(soundbank_18);
+EXTERN_SOUND(soundbank_19);
+EXTERN_SOUND(soundbank_20);
+EXTERN_SOUND(soundbank_21);
+EXTERN_SOUND(soundbank_22);
+EXTERN_SOUND(soundbank_23);
+EXTERN_SOUND(soundbank_24);
+
+#define BANK_ENTRY(type, nm) {&Sound_ ## nm ## _ ## type ## _ROM_START, &Sound_ ## nm ## _ ## type ## _ROM_END}
+BankTable WaveBankTables[] = {
+    BANK_ENTRY(wbk, effects),
+    BANK_ENTRY(wbk, voice),
+    BANK_ENTRY(wbk, sndfnt1),
+    BANK_ENTRY(wbk, sndfnt2),
+};
+
+BankTable PointerBankTables[] = {
+    BANK_ENTRY(ptr, effects),
+    BANK_ENTRY(ptr, voice),
+    BANK_ENTRY(ptr, sndfnt1),
+    BANK_ENTRY(ptr, sndfnt2),
+    BANK_ENTRY(tbl, effects),
+    BANK_ENTRY(tbl, voice),
+};
+
+BankTable SoundFXTables[] = {
+    BANK_ENTRY(bfx, soundbank_01),
+    BANK_ENTRY(bfx, soundbank_02),
+    BANK_ENTRY(bfx, soundbank_03),
+    BANK_ENTRY(bfx, soundbank_04),
+    BANK_ENTRY(bfx, soundbank_05),
+    BANK_ENTRY(bfx, soundbank_06),
+    BANK_ENTRY(bfx, soundbank_07),
+    BANK_ENTRY(bfx, soundbank_08),
+    BANK_ENTRY(bfx, soundbank_09),
+    BANK_ENTRY(bfx, soundbank_10),
+    BANK_ENTRY(bfx, soundbank_11),
+    BANK_ENTRY(bfx, soundbank_12),
+    BANK_ENTRY(bfx, soundbank_13),
+    BANK_ENTRY(bfx, soundbank_14),
+    BANK_ENTRY(bfx, soundbank_15),
+    BANK_ENTRY(bfx, soundbank_16),
+    BANK_ENTRY(bfx, soundbank_17),
+    BANK_ENTRY(bfx, soundbank_18),
+    BANK_ENTRY(bfx, soundbank_19),
+    BANK_ENTRY(bfx, soundbank_20),
+    BANK_ENTRY(bfx, soundbank_21),
+    BANK_ENTRY(bfx, soundbank_22),
+    BANK_ENTRY(bfx, soundbank_23),
+    BANK_ENTRY(bfx, soundbank_24),
+};
+
+int D_800D3B80 = -1;
+int D_800D3B84 = -1;
+float D_800D3B88 = 0;
+float D_800D3B8C = 0;
+float D_800D3B90 = 0;
+float D_800D3B94 = 0;
+float D_800D3B98 = 0;
+float D_800D3B9C = 256;
+u16 D_800D3BA0 = 0;
+u8 D_800D3BA2 = 0;
+
+
+
 INCLUDE_ASM("asm/nonmatchings/code_5A40", func_8009AE40);
 
 INCLUDE_ASM("asm/nonmatchings/code_5A40", func_8009B048);
@@ -9,10 +111,6 @@ INCLUDE_ASM("asm/nonmatchings/code_5A40", func_8009B268);
 
 INCLUDE_ASM("asm/nonmatchings/code_5A40", func_8009B43C);
 
-typedef struct {
-    u8 *romStart;
-    u8 *romEnd;
-} BankTable;
 extern BankTable PointerBankTables[];
 extern BankTable WaveBankTables[];
 extern BankTable SoundFXTables[];
