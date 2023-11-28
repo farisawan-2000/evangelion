9729d5ebcaf1e304d18b32e77b304ee5434d8ba6
someone2639 <someone2639@gmail.com>
2023-11-28 02:53:27 -0500

    cleaning

--- src/ovl3/code_000E1BE0.c
+++ src/ovl3/code_000E1BE0.c
@@ -1,7 +1,6 @@
 #include "common.h"
 #include "evangelion_types.h"
 
-// extern u16 *D_80068D40_ovl3;  // EVA_STR("目    標 :")
 extern u8 D_800D3BC0, D_800D3BC1, D_800D3BC2, D_800D3BC3, D_800D3BC4, D_800D3BC5, D_800D3BC6;
 extern f32 D_800D3BD8, D_800D3BDC, D_800D3BE0;
 
